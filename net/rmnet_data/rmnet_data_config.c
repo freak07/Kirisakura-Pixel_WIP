4fa2b95814617b594ec9a3a3d2df9c37301bb5c0
Ashwanth Goli <ashwanth@codeaurora.org>
2017-02-24 11:03:03 -0700

    net: rmnet_data: Unset logical end points in bridge mode
    We clean up the logical end points only for the un-registering device
    in bridge mode. However, the other physical end point's local end
    point still points to the the un-registered device.
    
    This may lead up to a crash if one of the physical devices in bridge
    mode is un-registered. Fix this by unsetting the local endpoint.
    
    It is still possible that packets in a different context across cores
    might try to access this data. This usually manifests as packets
    requesting a very large headroom. Handle this by dropping these stale
    skb's.
    
    CRs-Fixed: 1098513
    Change-Id: I1ba4d877a6ed3eca66946fe056938f0927bcd9a5
    Signed-off-by: Ashwanth Goli <ashwanth@codeaurora.org>
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
    (cherry picked from commit 3883bc1dbb2b9b8127b62cc08019e531ea8c5ffc)
    (cherry picked from commit 8e794b0b04a6b8c46ee7701fee939203c90a1d61)

--- net/rmnet_data/rmnet_data_config.c
+++ net/rmnet_data/rmnet_data_config.c
@@ -1118,6 +1118,7 @@ static void rmnet_force_unassociate_device(struct net_device *dev)
 {
 	int i, j;
 	struct net_device *vndev;
+	struct rmnet_phys_ep_conf_s *config;
 	struct rmnet_logical_ep_conf_s *cfg;
 	struct rmnet_free_vnd_work *vnd_work;
 	ASSERT_RTNL();
@@ -1171,6 +1172,16 @@ static void rmnet_force_unassociate_device(struct net_device *dev)
 		schedule_work(&vnd_work->work);
 	} else {
 		kfree(vnd_work);
+	}
+
+	config = _rmnet_get_phys_ep_config(dev);
+
+	if (config) {
+		cfg = &config->local_ep;
+
+		if (cfg && cfg->refcount)
+			rmnet_unset_logical_endpoint_config
+			(cfg->egress_dev, RMNET_LOCAL_LOGICAL_ENDPOINT);
 	}
 
 	/* Clear the mappings on the phys ep */
